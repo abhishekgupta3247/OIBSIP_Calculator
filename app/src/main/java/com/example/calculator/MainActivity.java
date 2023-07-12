f61a47ee4e598b3ddd3233270f7e709ddb0dcf49
Abhishek Gupta <abhishekgupta3247@gmail.com>
2023-07-12 17:54:19 +0530

    created TVs and MaterialButtons in MainActivity.java

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultTV, solutionTV;
+    MaterialButton buttonC, buttonBracketOpen, buttonBracketClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
